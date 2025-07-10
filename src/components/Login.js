43a6e1e074682a716c5294dd3ee5a53de2242005
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-07-11 03:37:38 +0530

    updated login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
-      <div className="absolute">
+      <div className="absolute h-screen w-full bg-red-700">
         <img
+          className="w-full h-full object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg"
           alt="body"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80">
+      <form className="w-full max-w-md z-10 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign Up" : "Sign In"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
@@ -22,8 +39,13 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-700"
         />
         <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
+        <p className="py-4 cursor-pointer" onClick={toggleForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now."
+            : "Already registered? Sign In now."}
+        </p>
       </form>
     </div>
   );
