9f0ab8ef5cbb3191e598afb840f5174421c00458
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-07-15 03:01:16 +0530

    authentication added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,11 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidateData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -13,9 +18,53 @@ const Login = () => {
   const handleButtonClick = () => {
     //validate the form data
     // checkValidateData;
-    setErrorMessage(
-      checkValidateData(email.current.value, password.current.value)
+    const message = checkValidateData(
+      email.current.value,
+      password.current.value
     );
+    setErrorMessage(message);
+
+    if (message) return;
+
+    if (!isSignInForm) {
+      //sign in logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //sign in logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
   };
 
   const toggleForm = () => {
@@ -36,7 +85,7 @@ const Login = () => {
         className="w-full max-w-md z-10 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80"
       >
         <h1 className="font-bold text-3xl py-4">
-          {isSignInForm ? "Sign Up" : "Sign In"}
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
